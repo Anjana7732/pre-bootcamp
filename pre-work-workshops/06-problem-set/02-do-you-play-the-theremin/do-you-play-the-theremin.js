8e3620a565ea14204cdf6c63aed0bae9ccdb5cb0
projectone <shreeshanjana768@gmail.com>
2025-07-08 18:39:56 +0545

    part6

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(string) {
+    if (string[0] === 'S'||string[0] ==='s') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(doYouPlayTheTheremin('sriffin'));
--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function maxOfThree(num1,num2,num3) {
+    if (num1>num2 && num2>num3){
+        return num1;
+    }
+    else if (num3>num2 && num2>num1) {
+        return num3;
+    } 
+    else return num2;
+
+}
+console.log(maxOfThree(4,7,13));
