bb685aa691987534f8721f6d64cb2a0f39a9dd1c
projectone <shreeshanjana768@gmail.com>
2025-07-13 14:03:50 +0545

    Implement deeperCopy function to create a nested copy of an array

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,4 +1,17 @@
 // YOUR CODE BELOW
 function deeperCopy(array) {
-    
+    let copy=[]
+    for (let i=0; i<array.length;i++) {
+        if (Array.isArray(array[i])) {
+            nestedcopy=[]
+            for(let j=0; j<array[i].length;j++) {
+                nestedcopy.push(array[i][j])
+            }
+            copy.push(nestedcopy)
+        }
+        else {
+            copy.push(array)
+        }
+    }
+    return copy
 }
